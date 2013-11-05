cc4ea9c8159c0c93986cba2f8075e52699f77ef7
Kostya Bazhanov <kostyabazhanov@mail.ru>
2013-11-05 19:43:49 +0400

    Script to deploy vim config

---
+++ deploy-vim-config.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+echo $'Removing existing .vimrc'
+rm ~/.vim
+echo $'Removing existing .vim/'
+rm -rf ~/.vim
+echo $'Copying new .vimrc'
+cp .vimrc ~
+echo $'Copying new .vim/'
+cp -r .vim ~
+echo $'Installing Vundle (see http://github.com/gmarik/vundle for more details'
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+echo $'To install rest of vim plugins enter :BundleList in Vim'
+echo $'And then press \'i\' on the line of each plugin you are interested in'
