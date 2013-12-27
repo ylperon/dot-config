6417784fa77dabd49f468671cb263c61ddb31358
Kostya Bazhanov <kostyabazhanov@mail.ru>
2013-12-27 21:42:05 +0400

    Fix messages

--- deploy-vim-config.sh
+++ deploy-vim-config.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
-echo $'Removing existing .vimrc'
+echo 'Removing existing .vimrc'
 rm ~/.vimrc
-echo $'Removing existing .vim/'
+echo 'Removing existing .vim/'
 rm -rf ~/.vim
-echo $'Copying new .vimrc'
+echo 'Copying new .vimrc'
 cp .vimrc ~
-echo $'Copying new .vim/'
+echo 'Copying new .vim/'
 cp -r .vim ~
-echo $'Installing Vundle (see http://github.com/gmarik/vundle for more details'
+echo 'Installing Vundle (see http://github.com/gmarik/vundle for more details'
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-echo $'To install rest of vim plugins enter :BundleList in Vim'
-echo $'And then press '"'"'i'"'"' on the line of each plugin you are interested in'
+echo 'To install rest of vim plugins enter :BundleList in Vim'
+echo 'And then press '"'"'i'"'"' on the line of each plugin you are interested in'
