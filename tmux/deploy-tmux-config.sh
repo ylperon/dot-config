bcb2534ca83b7d1365f98a330ea95dad48214d04
Kostya Bazhanov <kostyabazhanov@mail.ru>
2014-06-20 22:36:12 +0400

    Mac OS X specific configuration for tmux.

--- tmux/deploy-tmux-config.sh
+++ tmux/deploy-tmux-config.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env sh
 
-printf "%s" "Copying default tmux config..."
+printf "%s\n" "Configuring tmux:"
+
+printf "	%s" "Copying default tmux config..."
 cp -f tmux-common.conf ~/.tmux.conf
 printf "%s\n" "Done"
+
+platform=$(uname)
+if [[ $platform == *Darwin* ]]
+then
+    printf "	%s" "Configuring tmux for Mac OS X..."
+    cat tmux-mac-osx.conf >> ~/.tmux.conf
+    printf "%s\n" "Done"
+fi
+
+printf "%s\n" "Configuring tmux: Done"
