91cc9dc0b1ed29d23f590ee0f20ba008750a685a
Kostya Bazhanov <kostyabazhanov@mail.ru>
2014-08-10 13:56:43 +0400

    Use bash as shell.

--- tmux/deploy-tmux-config.sh
+++ tmux/deploy-tmux-config.sh
@@ -1,16 +1,17 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+set -e
 
 printf "%s\n" "Configuring tmux:"
 
 printf "	%s" "Copying default tmux config..."
-cp -f tmux-common.conf ~/.tmux.conf
+cp -f tmux-common.conf $HOME/.tmux.conf
 printf "%s\n" "Done"
 
 platform=$(uname)
 if [[ $platform == *Darwin* ]]
 then
     printf "	%s" "Configuring tmux for Mac OS X..."
-    cat tmux-mac-osx.conf >> ~/.tmux.conf
+    cat tmux-mac-osx.conf >> $HOME/.tmux.conf
     printf "%s\n" "Done"
 fi
 
