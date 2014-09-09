9f28d8b15399e8758aeafd038a1e9ec7654f3c83
Kostya Bazhanov <kostyabazhanov@mail.ru>
2014-09-09 21:27:56 +0400

    Remove colors from tmux-common and download them each time.

--- tmux/deploy.sh
+++ tmux/deploy.sh
@@ -1,22 +1,26 @@
-#!/usr/bin/env bash
+#!/bin/bash
 set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 printf 'Copying default Tmux config...\n'
-cp -f $DIR/tmux-common.conf $HOME/.tmux.conf
+cp -f "${DIR}/tmux-common.conf" "${HOME}/.tmux.conf"
 printf 'Copying default Tmux config...DONE\n'
 
-platform=$(uname)
-if [[ $platform == *Darwin* ]]
-then
+PLATFORM="$(uname)"
+if [[ "${PLATFORM}" == *Darwin* ]]; then
     printf 'Configuring Tmux for Mac OS X...\n'
-    cat $DIR/tmux-mac-osx.conf >> $HOME/.tmux.conf
+    cat "${DIR}/tmux-mac-osx.conf" >> "${HOME}/.tmux.conf"
     printf 'Configuring Tmux for Mac OS X...DONE\n'
 fi
 
-if ! { [ "$TERM" = 'screen' ] && [ -n "$TMUX" ]; } then
+printf 'Configurint Tmux colorscheme...\n'
+printf '\n' >> "${HOME}/.tmux.conf"
+TMUX_COLORSCHEME_URL='https://raw.githubusercontent.com/seebi/tmux-colors-solarized/master/tmuxcolors-256.conf'
+curl --fail --silent --show-error "${TMUX_COLORSCHEME_URL}" \
+    >> "${HOME}/.tmux.conf"
+printf 'Configurint Tmux colorscheme...DONE\n'
+
+if ! { [ "${TERM}" = 'screen' ] && [ -n "${TMUX}" ]; } then
     tmux source-file $HOME/.tmux.conf > /dev/null
 fi
-
-unset DIR
