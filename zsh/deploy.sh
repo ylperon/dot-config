1cfd04f9bab5d49ca02abe46a88c7a6aa640b96a
Kostya Bazhanov <kostyabazhanov@mail.ru>
2014-11-29 19:27:11 +0300

    Download oh-my-zsh if it wasn't downloaded before.

--- zsh/deploy.sh
+++ zsh/deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/zsh
 set -e
 
+if [ ! -d "${HOME}/.oh-my-zsh" ]; then
+    curl -L http://install.ohmyz.sh | sh
+fi
+
 DIR="$( cd "$( dirname "${(%):-%N}" )" && pwd )"
 
 cp "${DIR}/zshrc.sh" "${HOME}/.zshrc"
