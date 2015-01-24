47bd17f5ac28c7079d02f1d7da7221115fdc7ac5
Kostya Bazhanov <kostyabazhanov@mail.ru>
2015-01-24 19:31:57 +0300

    Formatting.

--- build_scripts/build_vim.sh
+++ build_scripts/build_vim.sh
@@ -76,10 +76,25 @@ download() {
 
     local target_direcory=$1
 
-    curl --fail --silent --show-error 'ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2' \
-        | tar --extract --bzip2 --file - --directory "${target_direcory}" &
-    curl --fail --silent --show-error 'ftp://invisible-island.net/ncurses/ncurses.tar.gz' \
-        | tar --extract --gzip --file - --directory "${target_direcory}" &
+    curl \
+        --fail \
+        --silent \
+        --show-error \
+        'ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2' \
+        | tar \
+            --extract \
+            --bzip2 \
+            --file - \
+            --directory "${target_direcory}" &
+    curl \
+        --fail \
+        --silent \
+        --show-error 'ftp://invisible-island.net/ncurses/ncurses.tar.gz' \
+        | tar \
+            --extract \
+            --gzip \
+            --file - \
+            --directory "${target_direcory}" &
     wait
 
     VIM_SOURCE_DIR="${target_direcory}/vim74"
@@ -102,7 +117,6 @@ build_ncurses() {
                 --silent
     eval "${make_tool}" -j
     eval "${make_tool}" install
-
 }
 
 build_vim() {
