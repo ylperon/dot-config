ba34901dec5b138ec79a37d202307747b6be8ae4
Kostya Bazhanov <kostyabazhanov@mail.ru>
2015-02-12 14:19:19 +0300

    Use sources from Vim trunk (or how it is called in Mercurial).

--- build_scripts/build_vim.sh
+++ build_scripts/build_vim.sh
@@ -76,7 +76,7 @@ download() {
 
     local target_direcory=$1
 
-    local vim_url='ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2'
+    local vim_url='https://vim.googlecode.com/archive/eaf81729ef02c04c6c7290f6532743a04d1bb8ac.tar.gz'
     local ncurses_url='ftp://invisible-island.net/ncurses/ncurses.tar.gz'
 
     curl \
@@ -86,7 +86,7 @@ download() {
         "${vim_url}" \
         | tar \
             --extract \
-            --bzip2 \
+            --gzip \
             --file - \
             --directory "${target_direcory}" &
     curl \
@@ -101,7 +101,7 @@ download() {
             --directory "${target_direcory}" &
     wait
 
-    VIM_SOURCE_DIR="${target_direcory}/vim74"
+    VIM_SOURCE_DIR="${target_direcory}/vim-eaf81729ef02"
     NCURCES_SOURCE_DIR="${target_direcory}/ncurses-5.9"
 }
 
