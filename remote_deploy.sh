30fcb1695e06ae79363e1d7a12bd6f2095efb911
Kostya Bazhanov <kostyabazhanov@mail.ru>
2015-04-29 00:30:03 +0300

    It is a TRAP!

--- remote_deploy.sh
+++ remote_deploy.sh
@@ -1,8 +1,12 @@
-#!/bin/bash
-set -e
+#!/bin/bash -e
 
 REPO_DIR="$(mktemp -d -t 'yazevnul-dot-config.XXXXXXXXXX')"
 
+cleanup() {
+    rm -rf "${REPO_DIR}"
+}
+
+trap cleanup EXIT
+
 git clone --quiet https://github.com/yazevnul/dot-config.git "${REPO_DIR}"
-"${REPO_DIR}/deploy.sh"
-rm -rf "${REPO_DIR}"
+eval "${REPO_DIR}/deploy.sh"
