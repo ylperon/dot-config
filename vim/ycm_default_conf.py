964d594dc865666d527096d8ad4fd209d81ee7ff
Kostya Bazhanov <kostyabazhanov@mail.ru>
2016-01-11 16:53:37 +0300

    Smart system headers retrieval

--- vim/ycm_default_conf.py
+++ vim/ycm_default_conf.py
@@ -1,6 +1,10 @@
-from os import uname
+import functools
+import inspect
+import os
+import re
+import subprocess
 
-FLAGS = [
+_BASE_FLAGS = [
     '-Weverything',
     '-std=c++14',
 
@@ -16,22 +20,71 @@ FLAGS = [
 ]
 
 
-def patch_flags_for_this_os():
-    global FLAGS
+def memoize(fn):
+    cache = fn.cache = {}
 
-    platform = uname()[0]
+    @functools.wraps(fn)
+    def memoizer(*args, **kwargs):
+        kwargs.update(dict(zip(inspect.getargspec(fn).args, args)))
+        key = tuple(kwargs.get(k, None) for k in inspect.getargspec(fn).args)
+        if key not in cache:
+            cache[key] = fn(**kwargs)
+        return cache[key]
+    return memoizer
 
+
+@memoize
+def _get_subprocess_output(cmd):
+    process = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                               stderr=subprocess.STDOUT)
+    output, unused_err = process.communicate('')
+    retcode = process.poll()
+    if retcode:
+        raise subprocess.CalledProcessError(retcode, cmd, output=output)
+
+    return output
+
+
+@memoize
+def retrieve_system_headers(compiler):
+    if 'clang' == compiler:
+        cmd = ('clang++', '-std=c++14', '-v', '-E', '-x', 'c++', '-', )
+    elif 'gcc' == compiler:
+        cmd = ('g++', '-std=c++14', '-v', '-E', '-x', 'c++', '-', )
+
+    output = _get_subprocess_output(cmd)
+    INCLUDES_RE = re.compile(
+        r'''^#include <\.\.\.> search starts here:\n(.+)\nEnd of search list''',
+        re.MULTILINE | re.DOTALL
+    )
+    includes = INCLUDES_RE.findall(output)
+    assert includes, 'Can\'t parse system includes'
+    assert 1 == len(includes), 'System includes are ambituious'
+    return [os.path.abspath(x.strip()) for x in includes[0].split('\n')]
+
+
+@memoize
+def get_extra_flags_for_this_os():
+    platform = os.uname()[0]
     if 'Darwin' == platform:
-        FLAGS.append('-stdlib=libc++')
-        FLAGS.append('-isystem')
-        FLAGS.append('/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1')
+        compiler = 'clang'
+    elif 'Linux' == platform:
+        compiler = 'gcc'
     else:
-        raise RuntimeError('Unknown platform!')
+        raise RuntimeError('Unknown platform')
+
+    extra_flags = retrieve_system_headers(compiler)
+    return extra_flags
+
+
+@memoize
+def get_flags():
+    return _BASE_FLAGS + get_extra_flags_for_this_os()
 
 
 def FlagsForFile(filename, **kwars):
-    patch_flags_for_this_os()
+    flags = get_flags()
 
-    return {'flags': FLAGS,
+    return {'flags': flags,
             'do_cache': True
             }
