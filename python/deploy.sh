bd3dd2d9eeb23c05e9ce91980961f3c621a71269
Kostya Bazhanov <kostyabazhanov@mail.ru>
2016-10-01 20:06:20 +0300

    update python deploy

--- python/deploy.sh
+++ python/deploy.sh
@@ -5,6 +5,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cp $DIR/python_startup.py $HOME/.python_startup
 $DIR/patch_profile.sh
-source $HOME/.profile
+source $HOME/.zshrc
 
 unset DIR
